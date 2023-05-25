1c071074fbedecb2f6e3c15f57c008fda2ff9488
Saidjon <saidjonsaidov707@gmail.com>
2023-05-25 07:09:20 -0500

    add all

---
+++ homework/Program.cs
@@ -0,0 +1,5 @@
+﻿using homework;
+
+var lasagna =  new Lasagna();
+var result = lasagna.ElapsedTimeInMinutes(3,20);
+Console.WriteLine(result);
---
+++ homework/lasagna.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework
+{
+    internal class Lasagna
+    {
+        public int ExpectedMinutesInOven()
+        {
+            return 40;
+        }
+        public int RemainingMinutesInOven(int minutes)
+        {
+            return 40 - minutes;
+        }
+
+        public int PreparationTimeInMinutes(int layers)
+        {
+            return layers*2;
+        }
+
+        public int ElapsedTimeInMinutes(int layers, int minutes)
+        {
+            var layer = layers * 2;
+            var left = 40 - minutes;
+            var result = left + layer;
+            return result;
+        }
+    }
+}
